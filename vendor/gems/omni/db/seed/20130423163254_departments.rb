1af3e44d99139784061aa58c1d2edea355c8068b
tunacasserole <aaron@highergroundsolutions.com>
2013-07-26 15:52:52 -0600

    seeds

---
+++ vendor/gems/omni/db/seed/20130423163254_departments.rb
@@ -0,0 +1,10 @@
+Omni::Department.delete_all
+Omni::Department.create(:department_id => "05A9CEBAAC5511E299E700FF58D32228",:display =>"ACCESSORIES",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05B62AFCAC5511E299E700FF58D32228",:display =>"ACTIVEWEAR",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05B874F6AC5511E299E700FF58D32228",:display =>"COMPONENTS",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05BA49B6AC5511E299E700FF58D32228",:display =>"FOOTWEAR",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05BC6CA0AC5511E299E700FF58D32228",:display =>"GIRLS SPECIALTY",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05BE687AAC5511E299E700FF58D32228",:display =>"KNIT TOPS",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05C03D44AC5511E299E700FF58D32228",:display =>"OUTERWEAR",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05C21204AC5511E299E700FF58D32228",:display =>"PANTS AND SHORTS",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
+Omni::Department.create(:department_id => "05C40DDEAC5511E299E700FF58D32228",:display =>"WOVEN TOPS",:department_nbr =>"",:description =>"",:short_name =>"",:buyer_user_id =>" ",:company_id =>"651A58C8AC3D11E2947800FF58D32228")
