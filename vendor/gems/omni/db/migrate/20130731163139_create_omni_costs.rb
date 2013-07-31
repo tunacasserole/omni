dc488a61e0b4657b9e323b80b85a4c848aa7e2a0
tunacasserole <aaron@highergroundsolutions.com>
2013-07-31 17:53:03 -0500

    deploy

--- vendor/gems/omni/db/migrate/20130731163139_create_omni_costs.rb
+++ vendor/gems/omni/db/migrate/20130731163139_create_omni_costs.rb
@@ -1,7 +1,7 @@
 class CreateOmniCosts < ActiveRecord::Migration
   def change
-  	ActiveRecord::Base.establish_connection(Buildit::Util::Data::Connection.for('BUILDIT'))
-    @connection = ActiveRecord::Base.connection
+  	# ActiveRecord::Base.establish_connection(Buildit::Util::Data::Connection.for('BUILDIT'))
+    #@connection = ActiveRecord::Base.connection
   	unless ActiveRecord::Base.connection.tables.include?('costs')
       create_table(:costs, :id => false) do |t|
         t.column   :cost_id,                         :string,            :null  =>  false,   :limit   => 32
@@ -16,6 +16,6 @@ class CreateOmniCosts < ActiveRecord::Migration
         t.column   :is_destroyed,                    :boolean,           :null  =>  true
       end
     end
-    ActiveRecord::Base.establish_connection(Buildit::Util::Data::Connection.for('BUILDIT'))
+    # ActiveRecord::Base.establish_connection(Buildit::Util::Data::Connection.for('BUILDIT'))
   end
 end
