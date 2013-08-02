a7ff918298a720da8ddc9f131bb758bc5eaa39ed
tunacasserole <aaron@highergroundsolutions.com>
2013-08-02 09:42:50 -0500

    demo

--- vendor/gems/omni/app/assets/javascripts/omni_desktop/view/sites/Explorer.js
+++ vendor/gems/omni/app/assets/javascripts/omni_desktop/view/sites/Explorer.js
@@ -1,70 +1,198 @@
 Ext.define('Omni.view.sites.Explorer', {
 
-  extend:'Buildit.ux.explorer.Panel',
-  alias:'widget.omni-sites-Explorer',
+  extend : 'Buildit.ux.explorer.Panel',
+  alias  : 'widget.omni-sites-Explorer',
 
-  
+  // EXPLORER INIT (Start) ===============================================================
+  store  : Ext.create('Omni.store.Site'),
 
-  initComponent:function () {
+  contextMenuConfig : {
+    xtype    : 'omni-sites-ExplorerContextMenu'
+  },
+
+  newForms : [{
+    xtype    : 'omni-sites-Form'
+  }],
+
+  inspectorConfig : {
+    xtype    : 'omni-sites-Inspector'
+  },
+  // EXPLORER INIT (End)
+
+  // LABELS (Start) ======================================================================
+  site_idLabel:                           Omni.i18n.model.Site.site_id,
+  displayLabel:                           Omni.i18n.model.Site.display,
+  site_nameLabel:                         Omni.i18n.model.Site.site_name,
+  parent_site_idLabel:                    Omni.i18n.model.Site.parent_site_id,
+  school_nbrLabel:                        Omni.i18n.model.Site.school_nbr,
+  descriptionLabel:                       Omni.i18n.model.Site.description,
+  short_nameLabel:                        Omni.i18n.model.Site.short_name,
+  concatenated_nameLabel:                 Omni.i18n.model.Site.concatenated_name,
+  site_typeLabel:                         Omni.i18n.model.Site.site_type,
+  location_idLabel:                       Omni.i18n.model.Site.location_id,
+  is_on_webLabel:                         Omni.i18n.model.Site.is_on_web,
+  gradesetLabel:                          Omni.i18n.model.Site.gradeset,
+  site_genderLabel:                       Omni.i18n.model.Site.site_gender,
+  line_1Label:                            Omni.i18n.model.Site.line_1,
+  line_2Label:                            Omni.i18n.model.Site.line_2,
+  line_3Label:                            Omni.i18n.model.Site.line_3,
+  line_4Label:                            Omni.i18n.model.Site.line_4,
+  cityLabel:                              Omni.i18n.model.Site.city,
+  state_codeLabel:                        Omni.i18n.model.Site.state_code,
+  zipLabel:                               Omni.i18n.model.Site.zip,
+  countryLabel:                           Omni.i18n.model.Site.country,
+  latitudeLabel:                          Omni.i18n.model.Site.latitude,
+  longitudeLabel:                         Omni.i18n.model.Site.longitude,
+  phoneLabel:                             Omni.i18n.model.Site.phone,
+  faxLabel:                               Omni.i18n.model.Site.fax,
+  is_destroyedLabel:                      Omni.i18n.model.Site.is_destroyed,
+  // LABELS (End)
+
+  // TITLES (Start) ======================================================================
+  title:     'Sites',
+  subtitle:  'Create and maintain Sites',
+  // TITLES (End)
+
+  initComponent : function () {
 
     var me = this;
 
-    
-    // EXPLORER INIT (Start) ===============================================================
-    Ext.apply(this, {
-
-      allowFind: true,
-
-      store: Ext.create('Omni.store.Site'),
-
-      contextMenuConfig:{
-        xtype:'omni-sites-ExplorerContextMenu',
-      },
-
-      newForms:[{
-        xtype:'omni-sites-Form',
-        windowConfig: {}
-      }],
-
-      inspectorConfig: {
-        xtype: 'omni-sites-Inspector'
-      }
-    });
-    // EXPLORER INIT (End)
-
-    // LABELS (Start) ======================================================================
-    Ext.applyIf(this, {
-      location_displayLabel:                 Omni.i18n.model.Site.location_display,
-      gradesetLabel:                         Omni.i18n.model.Site.gradeset,
-      site_genderLabel:                      Omni.i18n.model.Site.site_gender,
-      line_1Label:                           Omni.i18n.model.Site.line_1,
-      cityLabel:                             Omni.i18n.model.Site.city,
-      state_codeLabel:                       Omni.i18n.model.Site.state_code,
-      phoneLabel:                            Omni.i18n.model.Site.phone
-    });
-    // LABELS (End)
-
     // COLUMNS (Start) =====================================================================
     Ext.apply(this, {
-      columns: [
-        { header: this.location_displayLabel,                            dataIndex: 'location_display',                   flex: 1,   sortable: true  },
-        { header: this.gradesetLabel,                                    dataIndex: 'gradeset',                           flex: 1,   sortable: true , renderer: Buildit.util.Format.lookupRenderer('GRADESET') },
-        { header: this.site_genderLabel,                                 dataIndex: 'site_gender',                        flex: 1,   sortable: true , renderer: Buildit.util.Format.lookupRenderer('SITE_GENDER') },
-        { header: this.line_1Label,                                      dataIndex: 'line_1',                             flex: 1,   sortable: true  },
-        { header: this.cityLabel,                                        dataIndex: 'city',                               flex: 1,   sortable: true  },
-        { header: this.state_codeLabel,                                  dataIndex: 'state_code',                         flex: 1,   sortable: true , renderer: Buildit.util.Format.lookupRenderer('STATE_CODE') },
-        { header: this.phoneLabel,                                       dataIndex: 'phone',                              flex: 1,   sortable: true  }
+      columns      : [
+        {
+          header       : this.site_idLabel,
+          dataIndex    : 'site_id',
+          flex         : 1
+        },
+        {
+          header       : this.displayLabel,
+          dataIndex    : 'display',
+          flex         : 1
+        },
+        {
+          header       : this.site_nameLabel,
+          dataIndex    : 'site_name',
+          flex         : 1
+        },
+        {
+          header       : this.parent_site_idLabel,
+          dataIndex    : 'parent_site_id',
+          flex         : 1
+        },
+        {
+          header       : this.school_nbrLabel,
+          dataIndex    : 'school_nbr',
+          flex         : 1
+        },
+        {
+          header       : this.descriptionLabel,
+          dataIndex    : 'description',
+          flex         : 1
+        },
+        {
+          header       : this.short_nameLabel,
+          dataIndex    : 'short_name',
+          flex         : 1
+        },
+        {
+          header       : this.concatenated_nameLabel,
+          dataIndex    : 'concatenated_name',
+          flex         : 1
+        },
+        {
+          header       : this.site_typeLabel,
+          dataIndex    : 'site_type',
+          flex         : 1
+        },
+        {
+          header       : this.location_idLabel,
+          dataIndex    : 'location_id',
+          flex         : 1
+        },
+        {
+          header       : this.is_on_webLabel,
+          dataIndex    : 'is_on_web',
+          flex         : 1
+        },
+        {
+          header       : this.gradesetLabel,
+          dataIndex    : 'gradeset',
+          flex         : 1
+        },
+        {
+          header       : this.site_genderLabel,
+          dataIndex    : 'site_gender',
+          flex         : 1
+        },
+        {
+          header       : this.line_1Label,
+          dataIndex    : 'line_1',
+          flex         : 1
+        },
+        {
+          header       : this.line_2Label,
+          dataIndex    : 'line_2',
+          flex         : 1
+        },
+        {
+          header       : this.line_3Label,
+          dataIndex    : 'line_3',
+          flex         : 1
+        },
+        {
+          header       : this.line_4Label,
+          dataIndex    : 'line_4',
+          flex         : 1
+        },
+        {
+          header       : this.cityLabel,
+          dataIndex    : 'city',
+          flex         : 1
+        },
+        {
+          header       : this.state_codeLabel,
+          dataIndex    : 'state_code',
+          flex         : 1
+        },
+        {
+          header       : this.zipLabel,
+          dataIndex    : 'zip',
+          flex         : 1
+        },
+        {
+          header       : this.countryLabel,
+          dataIndex    : 'country',
+          flex         : 1
+        },
+        {
+          header       : this.latitudeLabel,
+          dataIndex    : 'latitude',
+          flex         : 1
+        },
+        {
+          header       : this.longitudeLabel,
+          dataIndex    : 'longitude',
+          flex         : 1
+        },
+        {
+          header       : this.phoneLabel,
+          dataIndex    : 'phone',
+          flex         : 1
+        },
+        {
+          header       : this.faxLabel,
+          dataIndex    : 'fax',
+          flex         : 1
+        },
+        {
+          header       : this.is_destroyedLabel,
+          dataIndex    : 'is_destroyed',
+          flex         : 1
+        }
       ]
     });
     // COLUMNS (End)
-
-    // TITLES (Start) ======================================================================
-    Ext.apply(this, {
-      title:     'Site',
-      subtitle:  'Each school covered by a contract'
-    });
-    // TITLES (End)
-
 
 
     this.callParent();
--- vendor/gems/omni/app/assets/javascripts/omni_desktop/view/sites/Form.js
+++ vendor/gems/omni/app/assets/javascripts/omni_desktop/view/sites/Form.js
@@ -3,38 +3,47 @@ Ext.define('Omni.view.sites.Form', {
   extend:'Buildit.ux.Form',
   alias:'widget.omni-sites-Form',
 
-  
 
   initComponent:function () {
 
     var me = this;
 
+    // FILTER (Start) =======================================================================
+    var associativeFilter = {
+      property:   'site_id',
+      value:      this.record.get('site_id')
+    };
+    // FILTER (End)
+
     
     // LABELS (Start) =======================================================================
     Ext.applyIf(this, {
-      site_nameLabel:                             Omni.i18n.model.Site.site_name,
-      parent_site_idLabel:                        Omni.i18n.model.Site.parent_site_id,
-      school_nbrLabel:                            Omni.i18n.model.Site.school_nbr,
-      descriptionLabel:                           Omni.i18n.model.Site.description,
-      short_nameLabel:                            Omni.i18n.model.Site.short_name,
-      concatenated_nameLabel:                     Omni.i18n.model.Site.concatenated_name,
-      site_typeLabel:                             Omni.i18n.model.Site.site_type,
-      location_idLabel:                           Omni.i18n.model.Site.location_id,
-      is_on_webLabel:                             Omni.i18n.model.Site.is_on_web,
-      gradesetLabel:                              Omni.i18n.model.Site.gradeset,
-      site_genderLabel:                           Omni.i18n.model.Site.site_gender,
-      line_1Label:                                Omni.i18n.model.Site.line_1,
-      line_2Label:                                Omni.i18n.model.Site.line_2,
-      line_3Label:                                Omni.i18n.model.Site.line_3,
-      line_4Label:                                Omni.i18n.model.Site.line_4,
-      cityLabel:                                  Omni.i18n.model.Site.city,
-      state_codeLabel:                            Omni.i18n.model.Site.state_code,
-      zipLabel:                                   Omni.i18n.model.Site.zip,
-      countryLabel:                               Omni.i18n.model.Site.country,
-      latitudeLabel:                              Omni.i18n.model.Site.latitude,
-      longitudeLabel:                             Omni.i18n.model.Site.longitude,
-      phoneLabel:                                 Omni.i18n.model.Site.phone,
-      faxLabel:                                   Omni.i18n.model.Site.fax
+      site_idLabel:                           Omni.i18n.model.Site.site_id,    
+      displayLabel:                           Omni.i18n.model.Site.display,    
+      site_nameLabel:                         Omni.i18n.model.Site.site_name,    
+      parent_site_idLabel:                    Omni.i18n.model.Site.parent_site_id,    
+      school_nbrLabel:                        Omni.i18n.model.Site.school_nbr,    
+      descriptionLabel:                       Omni.i18n.model.Site.description,    
+      short_nameLabel:                        Omni.i18n.model.Site.short_name,    
+      concatenated_nameLabel:                 Omni.i18n.model.Site.concatenated_name,    
+      site_typeLabel:                         Omni.i18n.model.Site.site_type,    
+      location_idLabel:                       Omni.i18n.model.Site.location_id,    
+      is_on_webLabel:                         Omni.i18n.model.Site.is_on_web,    
+      gradesetLabel:                          Omni.i18n.model.Site.gradeset,    
+      site_genderLabel:                       Omni.i18n.model.Site.site_gender,    
+      line_1Label:                            Omni.i18n.model.Site.line_1,    
+      line_2Label:                            Omni.i18n.model.Site.line_2,    
+      line_3Label:                            Omni.i18n.model.Site.line_3,    
+      line_4Label:                            Omni.i18n.model.Site.line_4,    
+      cityLabel:                              Omni.i18n.model.Site.city,    
+      state_codeLabel:                        Omni.i18n.model.Site.state_code,    
+      zipLabel:                               Omni.i18n.model.Site.zip,    
+      countryLabel:                           Omni.i18n.model.Site.country,    
+      latitudeLabel:                          Omni.i18n.model.Site.latitude,    
+      longitudeLabel:                         Omni.i18n.model.Site.longitude,    
+      phoneLabel:                             Omni.i18n.model.Site.phone,    
+      faxLabel:                               Omni.i18n.model.Site.fax,    
+      is_destroyedLabel:                      Omni.i18n.model.Site.is_destroyed    
     });
     // LABELS (End)
 
@@ -42,46 +51,53 @@ Ext.define('Omni.view.sites.Form', {
     Ext.apply(this, {
       items: [
         {
-          xtype: 'fieldset',
-          title: 'General',
-          collapsible: true,
-          defaultType: 'textfield',
-          defaults: {anchor: '70%'},
-          layout: 'anchor',
+          xtype:        'fieldset',
+          title:        'General Information',
+          collapsible:  true,
+          defaultType:  'textfield',
+          defaults:     {anchor: '95%'},
+          layout:       'anchor',
           items:[
-            { name: 'site_name',                      fieldLabel: this.site_nameLabel,                  allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'parent_site_id',                 fieldLabel: this.parent_site_idLabel,             allowBlank: true,   disabled: false,    xtype: 'buildit-Locator',     store:      Ext.create('Omni.store.Site',{pageSize: 10}), displayField: 'display', queryField: 'display', valueField: 'site_id', itemTpl:'{display}' },
-            { name: 'school_nbr',                     fieldLabel: this.school_nbrLabel,                 allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'description',                    fieldLabel: this.descriptionLabel,                allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'short_name',                     fieldLabel: this.short_nameLabel,                 allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'concatenated_name',              fieldLabel: this.concatenated_nameLabel,          allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'site_type',                      fieldLabel: this.site_typeLabel,                  allowBlank: false,  disabled: false,    xtype: 'buildit-Lookup',      category:   'SITE_TYPE' },
-            { name: 'location_id',                    fieldLabel: this.location_idLabel,                allowBlank: true,   disabled: false,    xtype: 'buildit-Locator',     store:      Ext.create('Omni.store.Location',{pageSize: 10}), displayField: 'display', queryField: 'display', valueField: 'location_id', itemTpl:'{display}' },
-            { name: 'is_on_web',                      fieldLabel: this.is_on_webLabel,                  allowBlank: true,   disabled: false,    xtype: 'checkbox'         },
-            { name: 'gradeset',                       fieldLabel: this.gradesetLabel,                   allowBlank: true,   disabled: false,    xtype: 'buildit-Lookup',      category:   'GRADESET' },
-            { name: 'site_gender',                    fieldLabel: this.site_genderLabel,                allowBlank: true,   disabled: false,    xtype: 'buildit-Lookup',      category:   'SITE_GENDER' }
-          ]
-        },
-        {
-          xtype: 'fieldset',
-          title: 'Contact Information',
-          collapsible: true,
-          defaultType: 'textfield',
-          defaults: {anchor: '70%'},
-          layout: 'anchor',
-          items:[
-            { name: 'line_1',                         fieldLabel: this.line_1Label,                     allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'line_2',                         fieldLabel: this.line_2Label,                     allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'line_3',                         fieldLabel: this.line_3Label,                     allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'line_4',                         fieldLabel: this.line_4Label,                     allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'city',                           fieldLabel: this.cityLabel,                       allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'state_code',                     fieldLabel: this.state_codeLabel,                 allowBlank: true,   disabled: true,     xtype: 'buildit-Lookup',      category:   'STATE_CODE' },
-            { name: 'zip',                            fieldLabel: this.zipLabel,                        allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'country',                        fieldLabel: this.countryLabel,                    allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'latitude',                       fieldLabel: this.latitudeLabel,                   allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'longitude',                      fieldLabel: this.longitudeLabel,                  allowBlank: true,   disabled: false,    xtype: 'textfield'        },
-            { name: 'phone',                          fieldLabel: this.phoneLabel,                      allowBlank: false,  disabled: false,    xtype: 'textfield'        },
-            { name: 'fax',                            fieldLabel: this.faxLabel,                        allowBlank: true,   disabled: false,    xtype: 'textfield'        }
+          /*
+            {
+              xtype: 'buildit-Locator', 
+              store: Ext.create('MyApp.store.MyModel',{pageSize: 10}), 
+              displayField: 'name', 
+              queryField: 'name', 
+              valueField: 'value_field', 
+              itemTpl:'{name}',
+              name: 'attribute_name', 
+              fieldLabel: this.attribute_nameLabel, 
+              allowBlank: true 
+            }
+          */
+
+            { xtype: 'textfield', name: 'site_id',                        fieldLabel: this.site_idLabel                     , allowBlank: false },    
+            { xtype: 'textfield', name: 'display',                        fieldLabel: this.displayLabel                     , allowBlank: false },    
+            { xtype: 'textfield', name: 'site_name',                      fieldLabel: this.site_nameLabel                   , allowBlank: false },    
+            { xtype: 'textfield', name: 'parent_site_id',                 fieldLabel: this.parent_site_idLabel              , allowBlank: false },    
+            { xtype: 'textfield', name: 'school_nbr',                     fieldLabel: this.school_nbrLabel                  , allowBlank: false },    
+            { xtype: 'textfield', name: 'description',                    fieldLabel: this.descriptionLabel                 , allowBlank: false },    
+            { xtype: 'textfield', name: 'short_name',                     fieldLabel: this.short_nameLabel                  , allowBlank: false },    
+            { xtype: 'textfield', name: 'concatenated_name',              fieldLabel: this.concatenated_nameLabel           , allowBlank: false },    
+            { xtype: 'textfield', name: 'site_type',                      fieldLabel: this.site_typeLabel                   , allowBlank: false },    
+            { xtype: 'textfield', name: 'location_id',                    fieldLabel: this.location_idLabel                 , allowBlank: false },    
+            { xtype: 'textfield', name: 'is_on_web',                      fieldLabel: this.is_on_webLabel                   , allowBlank: false },    
+            { xtype: 'textfield', name: 'gradeset',                       fieldLabel: this.gradesetLabel                    , allowBlank: false },    
+            { xtype: 'textfield', name: 'site_gender',                    fieldLabel: this.site_genderLabel                 , allowBlank: false },    
+            { xtype: 'textfield', name: 'line_1',                         fieldLabel: this.line_1Label                      , allowBlank: false },    
+            { xtype: 'textfield', name: 'line_2',                         fieldLabel: this.line_2Label                      , allowBlank: false },    
+            { xtype: 'textfield', name: 'line_3',                         fieldLabel: this.line_3Label                      , allowBlank: false },    
+            { xtype: 'textfield', name: 'line_4',                         fieldLabel: this.line_4Label                      , allowBlank: false },    
+            { xtype: 'textfield', name: 'city',                           fieldLabel: this.cityLabel                        , allowBlank: false },    
+            { xtype: 'textfield', name: 'state_code',                     fieldLabel: this.state_codeLabel                  , allowBlank: false },    
+            { xtype: 'textfield', name: 'zip',                            fieldLabel: this.zipLabel                         , allowBlank: false },    
+            { xtype: 'textfield', name: 'country',                        fieldLabel: this.countryLabel                     , allowBlank: false },    
+            { xtype: 'textfield', name: 'latitude',                       fieldLabel: this.latitudeLabel                    , allowBlank: false },    
+            { xtype: 'textfield', name: 'longitude',                      fieldLabel: this.longitudeLabel                   , allowBlank: false },    
+            { xtype: 'textfield', name: 'phone',                          fieldLabel: this.phoneLabel                       , allowBlank: false },    
+            { xtype: 'textfield', name: 'fax',                            fieldLabel: this.faxLabel                         , allowBlank: false },    
+            { xtype: 'textfield', name: 'is_destroyed',                   fieldLabel: this.is_destroyedLabel                , allowBlank: false }    
           ]
         }
       ]
@@ -89,7 +105,17 @@ Ext.define('Omni.view.sites.Form', {
     // FIELDSETS (End)
 
 
+    // TITLES (Start) ======================================================================
+    Ext.applyIf(this, {
+      title: 'Profile',
+      subtitle: 'Edit Sites',
+      newTitle: 'New Site',
+      newSubtitle: 'Complete the following to create a new Site'
+    });
+    // TITLES (End)
+
     this.callParent();
+    
   }
 
 });
