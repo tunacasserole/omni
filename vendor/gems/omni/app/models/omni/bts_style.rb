20577eace5160b2727f3b6b8b1504e6a192cfb5f
tunacasserole <aaron@highergroundsolutions.com>
2013-08-20 15:45:36 -0600

    deployment

--- vendor/gems/omni/app/models/omni/bts_style.rb
+++ vendor/gems/omni/app/models/omni/bts_style.rb
@@ -7,7 +7,7 @@ class Omni::BtsStyle < ActiveRecord::Base
 
 
   # METADATA (Start) ====================================================================
-  self.establish_connection       Buildit::Util::Data::Connection.for 'BUILDIT'
+  #self.establish_connection       Buildit::Util::Data::Connection.for 'BUILDIT'
   self.table_name                 = :bts_styles
   self.primary_key                = :bts_style_id
   # METADATA (End)
