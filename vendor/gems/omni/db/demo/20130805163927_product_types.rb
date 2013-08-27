32d20a1cf40e8661b525a559033c03e0a2f5b3ff
tunacasserole <aaron@highergroundsolutions.com>
2013-08-27 03:08:04 -0600

    master data

--- vendor/gems/omni/db/demo/20130805163927_product_types.rb
+++
@@ -1,4 +0,0 @@
-Omni::ProductType.delete_all
-Omni::ProductType.create(:product_type_id => 'B24D940CAC5611E299E700FF58D32228',:display => 'Generic Garment',:description => '',:short_name => '',:is_fabric => 0,:is_trim => 0,:is_converted => 0,:is_labor => 0,:is_shipping => 0,:is_destroyed => 0,)
-Omni::ProductType.create(:product_type_id => 'B25227F6AC5611E299E700FF58D32228',:display => 'Converted Garment',:description => '',:short_name => '',:is_fabric => 0,:is_trim => 0,:is_converted => 1,:is_labor => 0,:is_shipping => 0,:is_destroyed => 0,)
-Omni::ProductType.create(:product_type_id => 'B255836AAC5611E299E700FF58D32228',:display => 'Fabric',:description => '',:short_name => '',:is_fabric => 1,:is_trim => 0,:is_converted => 0,:is_labor => 0,:is_shipping => 0,:is_destroyed => 0,)
