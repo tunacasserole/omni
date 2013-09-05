296a34e3e6d5fa9ea84a7d05e49b081f139517b1
tunacasserole <aaron@highergroundsolutions.com>
2013-09-05 12:15:38 -0600

    BTS

--- lib/omni/archives/import/solr.rb
+++
@@ -1,8 +0,0 @@
-class Omni::Import::Solr < Omni::Import::Base
-
-  def self.run_all
-    Omni::Program.reindex
-  end  
-
-end
-
--- lib/omni/archives/meta/data.rb
+++
@@ -1,7 +0,0 @@
-class Omni::Meta::Data < Omni::Meta::Base
-  
-  def self.run(model)
-    Omni::Import::Manager.run model.model_name
-  end
-
-end
