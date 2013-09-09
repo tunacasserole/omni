e48500ae796f84e4af5a83afb275d8916962ac18
tunacasserole <aaron@highergroundsolutions.com>
2013-09-08 23:41:05 -0600

    seeds

---
+++ db/demo/20130908171214_regions.rb
@@ -0,0 +1,4 @@
+Omni::Region.delete_all
+Omni::Region.create(:region_id => 'B5F6FE68AC3D11E2947800FF58D32228',:display => 'Central Region',:region_nbr => '10',:description => 'Central includes Dallas, OK, MS, KS, Houston',:short_name => 'Central',:company_id => '651A58C8AC3D11E2947800FF58D32228',:user_id => ' ',:is_destroyed => 0,)
+Omni::Region.create(:region_id => 'B604932AAC3D11E2947800FF58D32228',:display => 'East Region',:region_nbr => '20',:description => 'East includes TN, GA, AL , KY',:short_name => 'East',:company_id => '651A58C8AC3D11E2947800FF58D32228',:user_id => ' ',:is_destroyed => 0,)
+Omni::Region.create(:region_id => 'B6068F04AC3D11E2947800FF58D32228',:display => 'West Region',:region_nbr => '30',:description => 'West includes CA, South TX',:short_name => 'West',:company_id => '651A58C8AC3D11E2947800FF58D32228',:user_id => ' ',:is_destroyed => 0,)
---
+++ db/demo/20130908171348_size_groups.rb
@@ -0,0 +1,12 @@
+Omni::SizeGroup.delete_all
+Omni::SizeGroup.create(:size_group_id => '636F7E9EAC5711E299E700FF58D32228',:display => 'LBY',:size_group_nbr => '1001',:description => '',:short_name => '',:concatenated_name => 'LBY',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '63712C58AC5711E299E700FF58D32228',:display => 'BBY',:size_group_nbr => '1002',:description => '',:short_name => '',:concatenated_name => 'BBY',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '63728BE8AC5711E299E700FF58D32228',:display => 'PBY',:size_group_nbr => '1003',:description => '',:short_name => '',:concatenated_name => 'PBY',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '6373EB82AC5711E299E700FF58D32228',:display => 'MEN',:size_group_nbr => '1004',:description => '',:short_name => '',:concatenated_name => 'MEN',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '6375722CAC5711E299E700FF58D32228',:display => 'HSK',:size_group_nbr => '1005',:description => '',:short_name => '',:concatenated_name => 'HSK',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '6376F8CCAC5711E299E700FF58D32228',:display => 'LGR',:size_group_nbr => '1006',:description => '',:short_name => '',:concatenated_name => 'LGR',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '63785866AC5711E299E700FF58D32228',:display => 'BGR',:size_group_nbr => '1007',:description => '',:short_name => '',:concatenated_name => 'BGR',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '637990E6AC5711E299E700FF58D32228',:display => 'JNR',:size_group_nbr => '1008',:description => '',:short_name => '',:concatenated_name => 'JNR',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '637AF080AC5711E299E700FF58D32228',:display => 'TGR',:size_group_nbr => '1009',:description => '',:short_name => '',:concatenated_name => 'TGR',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '637C2900AC5711E299E700FF58D32228',:display => 'UAD',:size_group_nbr => '1010',:description => '',:short_name => '',:concatenated_name => 'UAD',:is_destroyed => 0,)
+Omni::SizeGroup.create(:size_group_id => '637DAFAAAC5711E299E700FF58D32228',:display => 'UYH',:size_group_nbr => '1011',:description => '',:short_name => '',:concatenated_name => 'UYH',:is_destroyed => 0,)
