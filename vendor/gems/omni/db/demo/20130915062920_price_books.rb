b95f0b30564f95cebfa901e0147595c876bd4307
tunacasserole <aaron@highergroundsolutions.com>
2013-09-16 00:10:27 -0600

    finished master data

--- vendor/gems/omni/db/demo/20130915062920_price_books.rb
+++
@@ -1,6 +0,0 @@
-Omni::PriceBook.delete_all
-Omni::PriceBook.create(:price_book_id => 'B7BCB2EEAC5411E299E700FFNATGRITS',:display => 'True Grits Price Book',:description => 'True Grits Price Book',:price_book_type => nil,:short_name => nil,:is_destroyed => nil,)
-Omni::PriceBook.create(:price_book_id => 'B7BCB2EEAC5411E299E700FFNATIBUCK',:display => 'Buckhead Price Book',:description => 'Buckhead Price Book',:price_book_type => nil,:short_name => nil,:is_destroyed => nil,)
-Omni::PriceBook.create(:price_book_id => 'B7BCB2EEAC5411E299E700FFNATIONAL',:display => 'National Price Book',:description => 'National Price Book',:price_book_type => nil,:short_name => nil,:is_destroyed => nil,)
-Omni::PriceBook.create(:price_book_id => 'B7BCB2EEAC5411E299E700FFNATLOUIS',:display => 'Louisville Price Book',:description => 'Louisville Price Book',:price_book_type => nil,:short_name => nil,:is_destroyed => nil,)
-Omni::PriceBook.create(:price_book_id => 'B7BCB2EEAC5411E299E700FFNCHARTER',:display => 'Charter Price Book',:description => 'Charter Price Book',:price_book_type => nil,:short_name => nil,:is_destroyed => nil,)
