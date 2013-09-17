eab9111c1e3b09580b24e0e25ba23a86c9d62b86
tunacasserole <aaron@highergroundsolutions.com>
2013-09-17 13:06:13 -0600

    stuff

--- db/migrate/20130405002621_create_areas.rb
+++ db/migrate/20130405002621_create_areas.rb
@@ -3,11 +3,12 @@ class CreateAreas < ActiveRecord::Migration
     unless ActiveRecord::Base.connection.tables.include? 'areas'
       create_table(:areas, :id => false) do |t|
         t.column :area_id,                           :string,        :limit       => 32,     :null        => false
-        t.column :display,                           :string,        :limit       => 300,    :null        => false
-        t.column :location_id,                       :string,        :limit       => 32,     :null        => false
-        t.column :area_nbr,                          :string,        :limit       => 11,     :null        => false
+        t.column :display,                           :string,        :limit       => 300,    :null        => true
+        t.column :location_id,                       :string,        :limit       => 32,     :null        => true
+        t.column :area_nbr,                          :string,        :limit       => 11,     :null        => true
         t.column :description,                       :string,        :limit       => 300,    :null        => true
-        t.column :short_name,                        :string,        :limit       => 100,    :null        => false
+        t.column :short_name,                        :string,        :limit       => 100,    :null        => true
+        t.column :default_cube_capacity,             :decimal,       :precision   => 11,     :scale       => 2,      :null        => true
         t.column :is_receiving,                      :boolean,       :null        => true
         t.column :is_picking,                        :boolean,       :null        => true
         t.column :is_reserve,                        :boolean,       :null        => true
@@ -20,7 +21,6 @@ class CreateAreas < ActiveRecord::Migration
         t.column :is_quality_control,                :boolean,       :null        => true
         t.column :is_quick_case,                     :boolean,       :null        => true
         t.column :is_many_sku_per_bin,               :boolean,       :null        => true
-        t.column :default_cube_capacity,             :decimal,       :precision   => 11,     :scale       => 2,      :null        => true
         t.column :is_request_cube_calculation,       :boolean,       :null        => true
         t.column :last_utilization_calc_date,        :date,          :null        => true
         t.column :is_destroyed,                      :boolean,       :null        => true
