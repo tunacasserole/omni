b40317b802e8e4ba451b6bf27f06d1e48f4fb5f7
tomhendersonfl <tom@highergroundsolutions.com>
2013-10-23 11:37:06 -0400

    Updates 10/23

--- app/models/omni/projection_detail.rb
+++ app/models/omni/projection_detail.rb
@@ -13,11 +13,15 @@ class Omni::ProjectionDetail < ActiveRecord::Base
 
   # BEHAVIOR (Start) ====================================================================
   supports_audit
+  supports_fulltext
   # BEHAVIOR (End)
 
 
   # VALIDATIONS (Start) =================================================================
-  validates    :display,                         :presence    => true
+  validates    :display,                          :uniqueness  => true
+  validates    :projection_id,                    :presence    => true
+  validates    :sku_id,                           :presence    => true
+  validates    :location_id,                      :presence    => true
   # VALIDATIONS (End)
 
 
@@ -25,13 +29,12 @@ class Omni::ProjectionDetail < ActiveRecord::Base
   default      :projection_detail_id,             :override  =>  false,        :with  => :guid
   default      :display,                          :override  =>  false,        :to    => lambda{|m| "#{m.projection_display} - #{m.sku_display} - #{m.location_display}"}
   default      :projection_line_nbr,              :override  =>  false,        :with  => :sequence,         :named=>"PROJECTION_LINE_NBR"
-  default      :last_forecast_units,                   :override  =>  false,        :to    => 0
-  # default      :proposed_units,                   :override  =>  false,        :to    => 0
-  # default      :approved_units,                   :override  =>  false,        :to    => 0
-  # default      :sale_units_2012,                   :override  =>  false,        :to    => 0
-  # default      :sale_units_2011,                   :override  =>  false,        :to    => 0
-  # default      :sale_units_2010,                   :override  =>  false,        :to    => 0
-  # default      :sale_units_2013,                   :override  =>  false,        :to    => 0
+  default      :last_forecast_units,              :override  =>  false,        :to    => 0
+  default      :first_forecast_units,             :override  =>  false,        :to    => 0
+  default      :projection_1_units,               :override  =>  true,         :to    => 0
+  default      :projection_2_units,               :override  =>  true,         :to    => 0
+  default      :projection_3_units,               :override  =>  true,         :to    => 0
+  default      :projection_4_units,               :override  =>  true,         :to    => 0
   default      :is_destroyed,                     :override  =>  false,        :to    => false
   # DEFAULTS (End)
 
@@ -49,8 +52,9 @@ class Omni::ProjectionDetail < ActiveRecord::Base
   belongs_to   :projection,                      :class_name => 'Omni::Projection',              :foreign_key => 'projection_id'
   belongs_to   :sku,                             :class_name => 'Omni::Sku',                     :foreign_key => 'sku_id'
   belongs_to   :location,                        :class_name => 'Omni::Location',                :foreign_key => 'location_id'
+  belongs_to   :forecast_profile,                :class_name => 'Omni::ForecastProfile',         :foreign_key => 'forecast_profile_id'
+  belongs_to   :projection_location,             :class_name => 'Omni::ProjectionLocation',      :foreign_key => 'projection_location_id'
   # ASSOCIATIONS (End)
-
 
 
   # MAPPED ATTRIBUTES (Start) ===========================================================
@@ -62,12 +66,21 @@ class Omni::ProjectionDetail < ActiveRecord::Base
     map :style_display,                          :to => 'sku.style_display'
     map :color_id,                               :to => 'sku.color_id'
     map :color_display,                          :to => 'sku.color_display'
-    map :size_id,                               :to => 'sku.size_id'
-    map :size_display,                          :to => 'sku.size_display'
+    map :size_id,                                :to => 'sku.size_id'
+    map :size_display,                           :to => 'sku.size_display'
+    map :forecast_profile_display                :to => 'forecast_profile.display'
+    map :projection_location_display             :to => 'projection_location.display' if projection_location
   end
   # MAPPED ATTRIBUTES (End)
 
   # COMPUTED ATTRIBUTES (Start) =========================================================
+    computed_attributes do
+    compute :sale_units_ytd,                     :with => :compute_sale_units_ytd
+    compute :sale_units_py1,                     :with => :compute_sale_units_py1
+    compute :sale_units_py2,                     :with => :compute_sale_units_py2
+    compute :sale_units_py3,                     :with => :compute_sale_units_py3
+    compute :average_sales, :standard_deviation, :with => :compute_average_sales
+  end
   # COMPUTED ATTRIBUTES (End)
 
   # TEMPORARY ATTRIBUTES (Start) ========================================================
@@ -85,36 +98,55 @@ class Omni::ProjectionDetail < ActiveRecord::Base
 
   # INDEXING (Start) ====================================================================
   searchable do
-    string   :projection_display do projection.display if projection end
-    string   :sku_display do sku.display if sku end
-    string   :location_display do location.display if location end
+    # Exact match attributes
+    string   :projection_detail_id
+    string   :display 
     string   :projection_id
+    string   :projection_location_id
+    string   :forecast_profile_id
     string   :sku_id
+    string   :location_id
+    string   :sku_display 
+    string   :location_display
     string   :size_id
     string   :color_id
-    string   :location_id
     string   :style_id
-    # integer  :forecast_units
-    # integer  :proposed_units
-    # integer  :approved_units
-    string   :display
     string   :color_display do sku.color.display if sku and sku.color end
     string   :style_display do sku.style.display if sku and sku.style end
     string   :size_display do sku.size.display if sku and sku.size end
-
+    # Partial match attributes
     text     :size_display_fulltext, :using => :size_display
     text     :style_display_fulltext, :using => :style_display
     text     :color_display_fulltext, :using => :color_display
     text     :projection_display_fulltext, :using => :projection_display
     text     :sku_display_fulltext, :using => :sku_display
     text     :location_display_fulltext, :using => :location_display
-    # text     :forecast_units_fulltext, :using => :forecast_units
-    # text     :proposed_units_fulltext, :using => :proposed_units
-    # text     :approved_units_fulltext, :using => :approved_units
   end
   # INDEXING (End)
 
-
+  # HELPERS (Start) =====================================================================
+  def compute_sale_units_ytd
+    # read SkuPeriodResults for ProjectionDetail.sku_id, ProjectionDetail.location_id and current year. Sum regular_sale_units.
+  end
+  def compute_sale_units_py1
+    # read SkuPeriodResults for ProjectionDetail.sku_id, ProjectionDetail.location_id and current year - 1. Sum regular_sale_units.
+  end
+  def compute_sale_units_py2
+    # read SkuPeriodResults for ProjectionDetail.sku_id, ProjectionDetail.location_id and current year - 2. Sum regular_sale_units.
+  end
+  def compute_sale_units_py3
+    # read SkuPeriodResults for ProjectionDetail.sku_id, ProjectionDetail.location_id and current year -3. Sum regular_sale_units.
+  end
+  def compute_average_sales
+    avg = (self.sale_units_py1+self.sale_units_py2+self.sale_units_py3) / 3
+    dev = (((self.sale_units_py1 - avg)**2 + (self.sale_units_py2 - avg)**2 + (self.sale_units_py3 - avg)**2) / 3)
+#  Need to calculate square root of dev to get standard deviation
+    std = 0    
+    self.average_sales = avg
+    self.standard_deviation = std
+    self.save
+  end
+  # HELPERS (End)
 
   # STATES (Start) ====================================================================
 
