78e833a9be5cf705be8f49eca0fffe4631acd709
tunacasserole <aaron@highergroundsolutions.com>
2013-11-15 04:09:10 -0700

    generic message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Omni::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = true # to fix heroku issues
 
   # Generate digests for assets URLs.
   config.assets.digest = true
