73c8dfc721bf3eae48ce28bcc696a95f40c142c2
tunacasserole <aaron@highergroundsolutions.com>
2013-11-19 15:20:39 -0700

    generic message

--- app/models/omni/supplier.rb
+++ app/models/omni/supplier.rb
@@ -1,10 +1,5 @@
 class Omni::Supplier < ActiveRecord::Base
 
-  # MIXINS (Start) ======================================================================
-
-  # MIXINS (End)
-
-
   # METADATA (Start) ====================================================================
   self.table_name   = :suppliers
   self.primary_key  = :supplier_id
@@ -12,53 +7,50 @@ class Omni::Supplier < ActiveRecord::Base
 
 
   # BEHAVIOR (Start) ====================================================================
-  #supports_logical_delete
-  #supports_audit
-  #supports_revisioning
   supports_fulltext
   # BEHAVIOR (End)
 
 
   # VALIDATIONS (Start) =================================================================
-  validates    :supplier_name,                   :uniqueness  => true,         :allow_nil => false 
+  validates    :supplier_name,                   :uniqueness  => true,         :allow_nil => false
   validates    :supplier_nbr,                    :uniqueness  => true
   # VALIDATIONS (End)
 
 
   # DEFAULTS (Start) ====================================================================
-  default      :supplier_id,                      :override  =>  false,        :with  => :guid              
+  default      :supplier_id,                      :override  =>  false,        :with  => :guid
   default      :display,                          :override  =>  false,        :to    => lambda{|m| "#{m.supplier_name}"}
   default      :supplier_nbr,                     :override  =>  false,        :with  => :sequence,         :named=>"SUPPLIER_NBR"
-  default      :supplier_ucc_prefix,              :override  =>  false,        :to    => 0                  
-  default      :duns_number,                      :override  =>  false,        :to    => 0                  
-  default      :minimum_order_cost,               :override  =>  false,        :to    => 0                  
-  default      :minimum_order_units,              :override  =>  false,        :to    => 0                  
-  default      :minimum_weight,                   :override  =>  false,        :to    => 0                  
-  default      :minimum_cube,                     :override  =>  false,        :to    => 0                  
-  default      :is_ship_cancel,                   :override  =>  false,        :to    => false              
-  default      :lead_time,                        :override  =>  false,        :to    => 0                  
-  default      :safety_stock_days,                :override  =>  false,        :to    => 0                  
-  default      :is_calculated_lead_time,          :override  =>  false,        :to    => false              
-  default      :is_dynamic_safety_stock,          :override  =>  false,        :to    => false              
+  default      :supplier_ucc_prefix,              :override  =>  false,        :to    => 0
+  default      :duns_number,                      :override  =>  false,        :to    => 0
+  default      :minimum_order_cost,               :override  =>  false,        :to    => 0
+  default      :minimum_order_units,              :override  =>  false,        :to    => 0
+  default      :minimum_weight,                   :override  =>  false,        :to    => 0
+  default      :minimum_cube,                     :override  =>  false,        :to    => 0
+  default      :is_ship_cancel,                   :override  =>  false,        :to    => false
+  default      :lead_time,                        :override  =>  false,        :to    => 0
+  default      :safety_stock_days,                :override  =>  false,        :to    => 0
+  default      :is_calculated_lead_time,          :override  =>  false,        :to    => false
+  default      :is_dynamic_safety_stock,          :override  =>  false,        :to    => false
   # default      :bank_routing_nbr,                 :override  =>  false,        :with  => :sequence,         :named=>"BANK_ROUTING_NBR"
-  default      :tax_identifier,                   :override  =>  false,        :to    => 0                  
-  default      :is_required_1099,                 :override  =>  false,        :to    => false              
-  default      :is_edi_capable,                   :override  =>  false,        :to    => false              
-  default      :is_one_time,                      :override  =>  false,        :to    => false              
-  default      :is_employee,                      :override  =>  false,        :to    => false              
-  default      :is_payee,                         :override  =>  false,        :to    => false              
-  default      :is_merchandise,                   :override  =>  false,        :to    => false              
-  default      :is_supply,                        :override  =>  false,        :to    => false              
-  default      :is_expense,                       :override  =>  false,        :to    => false              
-  default      :is_creditor,                      :override  =>  false,        :to    => false              
-  default      :is_freight,                       :override  =>  false,        :to    => false              
-  default      :is_factory,                       :override  =>  false,        :to    => false              
-  default      :is_3pl,                           :override  =>  false,        :to    => false              
-  default      :is_agent,                         :override  =>  false,        :to    => false              
-  default      :is_outbound_shipper,              :override  =>  false,        :to    => false              
-  default      :is_on_payment_hold,               :override  =>  false,        :to    => false              
-  default      :is_enabled,                       :override  =>  false,        :to    => false              
-  default      :is_destroyed,                     :override  =>  false,        :to    => false              
+  default      :tax_identifier,                   :override  =>  false,        :to    => 0
+  default      :is_required_1099,                 :override  =>  false,        :to    => false
+  default      :is_edi_capable,                   :override  =>  false,        :to    => false
+  default      :is_one_time,                      :override  =>  false,        :to    => false
+  default      :is_employee,                      :override  =>  false,        :to    => false
+  default      :is_payee,                         :override  =>  false,        :to    => false
+  default      :is_merchandise,                   :override  =>  false,        :to    => false
+  default      :is_supply,                        :override  =>  false,        :to    => false
+  default      :is_expense,                       :override  =>  false,        :to    => false
+  default      :is_creditor,                      :override  =>  false,        :to    => false
+  default      :is_freight,                       :override  =>  false,        :to    => false
+  default      :is_factory,                       :override  =>  false,        :to    => false
+  default      :is_3pl,                           :override  =>  false,        :to    => false
+  default      :is_agent,                         :override  =>  false,        :to    => false
+  default      :is_outbound_shipper,              :override  =>  false,        :to    => false
+  default      :is_on_payment_hold,               :override  =>  false,        :to    => false
+  default      :is_enabled,                       :override  =>  false,        :to    => false
+  default      :is_destroyed,                     :override  =>  false,        :to    => false
   # DEFAULTS (End)
 
 
@@ -78,7 +70,7 @@ class Omni::Supplier < ActiveRecord::Base
   has_many     :notes,                           :class_name => 'Buildit::Note',                 :foreign_key => 'notable_id',       :as => :notable
   has_many     :style_suppliers,                 :class_name => 'Omni::StyleSupplier',           :foreign_key => 'supplier_id'
   has_many     :sku_suppliers,                   :class_name => 'Omni::SkuSupplier',             :foreign_key => 'supplier_id'
-  has_many     :purchases,                       :class_name => 'Omni::Purchase',                :foreign_key => 'supplier_id'  
+  has_many     :purchases,                       :class_name => 'Omni::Purchase',                :foreign_key => 'supplier_id'
   # ASSOCIATIONS (End)
 
 
@@ -120,7 +112,7 @@ class Omni::Supplier < ActiveRecord::Base
     # string   :phone
     # string   :fax
     string   :display
- 
+
     text     :display_fulltext, :using => :display
     text     :supplier_nbr_fulltext, :using => :supplier_nbr
     text     :supplier_name_fulltext, :using => :supplier_name
@@ -132,7 +124,7 @@ class Omni::Supplier < ActiveRecord::Base
     # text     :zip_fulltext, :using => :zip
     # text     :phone_fulltext, :using => :phone
     # text     :fax_fulltext, :using => :fax
-  end 
+  end
   # INDEXING (End)
 
 end # class Omni::Supplier
