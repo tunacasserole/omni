3bf0e23857eb0f5b2464ca56631875e7cfb56c12
Jason Ihaia <jason@ihaia.com>
2014-04-01 16:25:15 -0600

    initial support for projections subscriber

---
+++ app/subscribers/omni/projections_subscriber.rb
@@ -0,0 +1,21 @@
+class Omni::ProjectionsSubscriber < Buildit::Messaging::Subscriber
+
+  queue       'model.forecast_requests'
+  exchange    'omni.events'
+  #routing_key 'forecast'
+  auto_start  true
+
+  def self.process(delivery_info, properties, message)
+    begin
+      msg      = JSON.parse(message)
+      user_id  = msg[:user_id]
+
+      projection = Omni::Projection.find(msg[:projection_id])
+      projection.forecast(user_id)
+      
+    rescue Exception => e
+      logger.error (e.message || "").red
+    end
+  end
+
+end # class Omni::ProjectionsSubscriber
--- config/initializers/buildit.rb
+++ config/initializers/buildit.rb
@@ -9,6 +9,7 @@ Buildit::Framework.configure do
   config.model_paths            += [File.join(Rails.root, "app/models",   "**")]
   config.email_template_paths   += [File.join(Rails.root, "app/emails",   "**")]
   config.event_template_paths   += [File.join(Rails.root, "app/events",   "**")]
+  config.subscriber_paths       += [File.join(Rails.root, "app/subscribers", "**")]
 end # Buildit::Framework.configure
 
 Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
