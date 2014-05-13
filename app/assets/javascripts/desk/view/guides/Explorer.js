089e76210ee1b70a7638a71f7907829af78a618d
tunacasserole <aaron@highergroundsolutions.com>
2014-05-12 18:02:45 -0600

    generic message

--- app/assets/javascripts/desk/view/guides/Explorer.js
+++ app/assets/javascripts/desk/view/guides/Explorer.js
@@ -1,86 +1,91 @@
 Ext.define('Desk.view.guides.Explorer', {
 
-  extend   : 'Buildit.ux.explorer.Panel',
-  alias    : 'widget.desk-guides-Explorer',
+  extend: 'Buildit.ux.explorer.Panel',
+  alias: 'widget.desk-guides-Explorer',
 
   // EXPLORER INIT (Start) ===============================================================
-  contextMenuConfig : {
-    xtype    : 'buildit-explorer-ContextMenu'
+  contextMenuConfig: {
+    xtype: 'buildit-explorer-ContextMenu'
   },
 
-  newForms : [{
-    xtype    : 'desk-guides-Form'
+  newForms: [{
+    xtype: 'desk-guides-Form'
   }],
 
-  inspectorConfig : {
-    xtype    : 'desk-guides-Inspector'
+  inspectorConfig: {
+    xtype: 'desk-guides-Inspector'
   },
   // EXPLORER INIT (End)
 
   // LABELS (Start) ======================================================================
-  guide_idLabel                           : Desk.i18n.model.Guide.guide_id,
-  owner_idLabel                           : Desk.i18n.model.Guide.owner_id,
-  guide_nbrLabel                          : Desk.i18n.model.Guide.guide_nbr,
-  guide_nameLabel                         : Desk.i18n.model.Guide.guide_name,
-  descriptionLabel                        : Desk.i18n.model.Guide.description,
-  guide_locationLabel                     : Desk.i18n.model.Guide.guide_location,
-  gem_nameLabel                           : Desk.i18n.model.Guide.gem_name,
+  guide_idLabel: Desk.i18n.model.Guide.guide_id,
+  owner_idLabel: Desk.i18n.model.Guide.owner_id,
+  guide_nbrLabel: Desk.i18n.model.Guide.guide_nbr,
+  guide_nameLabel: Desk.i18n.model.Guide.guide_name,
+  descriptionLabel: Desk.i18n.model.Guide.description,
+  guide_locationLabel: Desk.i18n.model.Guide.guide_location,
+  gem_nameLabel: Desk.i18n.model.Guide.gem_name,
   // LABELS (End)
 
   // TITLES (Start) ======================================================================
-  title    : 'Guides',
-  subtitle : 'Create and maintain Guides',
+  title: 'Guides',
+  subtitle: 'Create and maintain Guides',
   // TITLES (End)
 
-  initComponent : function () {
+  initComponent: function() {
 
     var me = this;
 
     // CONFIG (Start) ======================================================================
     Ext.apply(me, {
-      store        : Ext.create('Desk.store.Guide')
+      store: Ext.create('Desk.store.Guide')
     });
     // CONFIG (End)
 
     // COLUMNS (Start) =====================================================================
     Ext.apply(me, {
-      columns      : [
-    // {
-    //   header       : me.owner_idLabel,
-    //   dataIndex    : 'owner_id',
-    //   flex         : 1
-    // },
-    {
-      header       : me.guide_nbrLabel,
-      dataIndex    : 'guide_nbr',
-      flex         : 1
-    },
-    {
-      header       : me.guide_nameLabel,
-      dataIndex    : 'guide_name',
-      flex         : 3
-    },
-    {
-      header       : me.descriptionLabel,
-      dataIndex    : 'description',
-      flex         : 5
-    }
-    // {
-    //   header       : me.guide_locationLabel,
-    //   dataIndex    : 'guide_location',
-    //   flex         : 1
-    // },
-    // {
-    //   header       : me.gem_nameLabel,
-    //   dataIndex    : 'gem_name',
-    //   flex         : 1
-    // }
+      columns: [
+        // {
+        //   header       : me.owner_idLabel,
+        //   dataIndex    : 'owner_id',
+        //   flex         : 1
+        // },
+        {
+          header: me.guide_nbrLabel,
+          dataIndex: 'guide_nbr',
+          flex: 1
+        }, {
+          header: me.guide_nameLabel,
+          dataIndex: 'guide_name',
+          flex: 2
+        }, {
+          header: me.guide_locationLabel,
+          dataIndex: 'guide_location',
+          flex: 3,
+          renderer: this.formatURL
+        }, {
+          header: me.descriptionLabel,
+          dataIndex: 'description',
+          flex: 4
+        },
+        // {
+        //   header       : me.gem_nameLabel,
+        //   dataIndex    : 'gem_name',
+        //   flex         : 1
+        // }
       ]
     });
     // COLUMNS (End)
 
-
     me.callParent();
+  },
+
+  formatURL: function(value) {
+    myURL = '';
+    if (value !== '') {
+      myURL = '<a href="' + value + '" target="_blank">' + value + '</a>';
+    }
+    return myURL;
   }
 
 });
