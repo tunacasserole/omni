bc0e3017cade60997e3ae62eb7cd67273ca02031
tunacasserole <aaron@highergroundsolutions.com>
2014-05-13 11:18:50 -0600

    generic message

--- app/assets/javascripts/desk/view/guides/Inspector.js
+++ app/assets/javascripts/desk/view/guides/Inspector.js
@@ -1,64 +1,63 @@
 Ext.define('Desk.view.guides.Inspector', {
-    extend: 'Buildit.ux.inspector.Panel',
-    alias: 'widget.desk-guides-Inspector',
+  extend: 'Buildit.ux.inspector.Panel',
+  alias: 'widget.desk-guides-Inspector',
 
 
-    initComponent: function() {
-      var me = this;
+  initComponent: function() {
+    var me = this;
 
-      // INSPECTOR INIT (Start) ==============================================================
-      Ext.applyIf(this, {
-        associativeFilter: {
-          property: 'guide_id',
-          value: this.record.get('guide_id')
-        },
+    // INSPECTOR INIT (Start) ==============================================================
+    Ext.applyIf(this, {
+      associativeFilter: {
+        property: 'guide_id',
+        value: this.record.get('guide_id')
+      },
 
-        associativeSearch: {
-          with: {
-            guide_id: {
-              equal_to: this.record.get('guide_id')
-            }
+      associativeSearch: {
+        with: {
+          guide_id: {
+            equal_to: this.record.get('guide_id')
           }
         }
-      });
-      // INSPECTOR INIT (End)
+      }
+    });
+    // INSPECTOR INIT (End)
 
-      // CARDS (Start) =======================================================================
-      Ext.apply(this, {
-          cards: [{
-              title: 'Profile',
-              xtype: 'desk-guides-Form'
+    // CARDS (Start) =======================================================================
+    Ext.apply(this, {
+      cards: [{
+        title: 'Profile',
+        xtype: 'desk-guides-Form'
+      }, {
+        title: 'Notes',
+        xtype: 'buildit-notes-Explorer',
+        defaultSearch: {
+          with: {
+            notable_type: {
+              equal_to: 'Desk::Guide'
             },
-            title: 'Notes',
-            xtype: 'buildit-notes-Explorer',
-            defaultSearch: {
-              with: {
-                notable_type: {
-                  equal_to: 'Desk::Guide'
-                },
-                notable_id: {
-                  equal_to: me.record.get('guide_id')
-                }
-              }
+            notable_id: {
+              equal_to: me.record.get('guide_id')
+            }
+          }
+        },
+        showBadge: true
+      }, {
+        title: 'Attachments',
+        xtype: 'buildit-attachments-Explorer',
+        defaultSearch: {
+          with: {
+            attachable_type: {
+              equal_to: 'Desk::Guide'
             },
-            showBadge: true
-          },
-          {
-            title: 'Attachments',
-            xtype: 'buildit-attachments-Explorer',
-            defaultSearch: {
-              with: {
-                attachable_type: {
-                  equal_to: 'Desk::Guide'
-                },
-                attachable_id: {
-                  equal_to: me.record.get('guide_id')
-                }
-              }
-            },
-            showBadge: true
-          }]
-      });
+            attachable_id: {
+              equal_to: me.record.get('guide_id')
+            }
+          }
+        },
+        showBadge: true
+      }]
+    });
     // CARDS (End)
 
     // TITLES (Start) ======================================================================
