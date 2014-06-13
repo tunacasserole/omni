3ce8f83d9b5c7a32b0bd433ab3684ad3dd936474
tunacasserole <aaron@highergroundsolutions.com>
2014-06-13 10:56:49 -0600

    generic message

--- lib/omni/data/sync/uniform.rb
+++ lib/omni/data/sync/uniform.rb
@@ -4,6 +4,7 @@ class Omni::Data::Sync::Uniform
     Omni::Account.all.each do |x|
       x.uniforms.create(state: 'active', school_year: '2015', uniform_name: '2015') unless x.uniforms.where(school_year: '2015').first
     end
+    Sunspot.commit_if_dirty
   end
 
 end
